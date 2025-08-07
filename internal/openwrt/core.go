86e58919169dd0d2442902e595b3cb470205c9eb
M4MacBookAir <M4MacBookAir@clife.cn>
2025-08-07 17:31:13 +0800

    发布版本 v0.0.73

--- internal/openwrt/core.go
+++ internal/openwrt/core.go
@@ -15,8 +15,10 @@ import (
 	"os/exec"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strconv"
 	"strings"
+	"syscall"
 	"time"
 )
 
@@ -480,6 +482,43 @@ func parseTimer(logLine string) (*time.Time, error) {
 //	return Cmd(context.Background(), exitFun, fu, name, arg...)
 //}
 
+func writePid(name string, pid int) {
+	pidData := []byte(strconv.Itoa(pid))
+	if err := os.WriteFile(filepath.Join(glog.AppHome("pid"), name), pidData, 0644); err != nil {
+		glog.Error("写入PID文件失败:", err)
+	}
+}
+
+func readPid(name string) int {
+	data, err := os.ReadFile(filepath.Join(glog.AppHome("pid"), name))
+	if err != nil {
+		glog.Error("读取PID失败:", err)
+		return 0
+	}
+	savedPid, err := strconv.Atoi(string(data))
+	if err != nil {
+		return 0
+	}
+	return savedPid
+}
+
+func killPid(pid int) {
+	if pid == 0 {
+		return
+	}
+	// 4. 根据PID终止进程
+	if runtime.GOOS == "windows" {
+		killCmd := exec.Command("taskkill", "/F", "/PID", strconv.Itoa(pid))
+		if err := killCmd.Run(); err != nil {
+			log.Fatal("终止失败:", err)
+		}
+	} else {
+		if err := syscall.Kill(pid, syscall.SIGKILL); err != nil {
+			log.Fatal("终止失败:", err)
+		}
+	}
+}
+
 func Command(ctx context.Context, fu func(string), name string, arg ...string) error {
 	glog.Println(name, arg)
 	//ctx, cancel := context.WithCancel(context.Background())
@@ -499,6 +538,10 @@ func Command(ctx context.Context, fu func(string), name string, arg ...string) e
 		fmt.Printf("启动命令失败: %v\n", e)
 		return err
 	}
+	procName := fmt.Sprintf("%s%s", name, strings.Join(arg, "."))
+	pid := readPid(procName)
+	killPid(pid)
+	writePid(procName, ccc.Process.Pid)
 	defer ccc.Process.Kill() // 确保退出时终止进程
 
 	// 实时读取输出流
