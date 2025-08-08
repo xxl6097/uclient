64dd05d7090766dac8eeb8a0219ac5c8b5430dbb
M4MacBookAir <M4MacBookAir@clife.cn>
2025-08-08 19:46:34 +0800

    发布版本 v0.0.93

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 module=$(grep "module" go.mod | cut -d ' ' -f 2)
 appname=$(basename $module)
 #options=("windows:amd64" "windows:arm64" "linux:amd64" "linux:arm64" "linux:arm:7" "linux:arm:5" "linux:mips64" "linux:mips64le" "linux:mips:softfloat" "linux:mipsle:softfloat" "linux:riscv64" "linux:loong64" "darwin:amd64" "darwin:arm64" "freebsd:amd64" "android:arm64")
-options=("linux:arm64" "linux:amd64" "linux:arm:7" "linux:arm:5" "linux:mips64" "linux:mips64le" "linux:mips:softfloat" "linux:mipsle:softfloat" "linux:riscv64" "linux:loong64")
-#options=("linux:amd64")
+#options=("linux:arm64" "linux:amd64" "linux:arm:7" "linux:arm:5" "linux:mips64" "linux:mips64le" "linux:mips:softfloat" "linux:mipsle:softfloat" "linux:riscv64" "linux:loong64")
+options=("linux:arm64")
 #options=("linux:amd64" "windows:amd64")
 version=$(git tag -l "v[0-99]*.[0-99]*.[0-99]*" --sort=-creatordate | head -n 1)
 versionDir="$module/pkg"
