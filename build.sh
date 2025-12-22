678a660257efe7b58b8745a8bbbce970a047d0b9
cnb <gQCz49EeeAgaUSex5aNIfd+uuxia@noreply.cnb.cool>
2025-12-22 19:44:57 +0800

    【v0.2.4】by uuxia 2025-12-22 19:44:57

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ appname=$(basename $module)
 #options=("windows:amd64" "windows:arm64" "linux:amd64" "linux:arm64" "linux:arm:7" "linux:arm:5" "linux:mips64" "linux:mips64le" "linux:mips:softfloat" "linux:mipsle:softfloat" "linux:riscv64" "linux:loong64" "darwin:amd64" "darwin:arm64" "freebsd:amd64" "android:arm64")
 #options=("linux:arm64" "linux:amd64" "linux:arm:7" "linux:arm:5" "linux:mips64" "linux:mips64le" "linux:mips:softfloat" "linux:mipsle:softfloat" "linux:riscv64" "linux:loong64")
 #options=("linux:arm64")
-options=("linux:amd64" "windows:arm64")
+options=("linux:amd64" "linux:arm64")
 version=$(git tag -l "v[0-99]*.[0-99]*.[0-99]*" --sort=-creatordate | head -n 1)
 #version="v0.1.14"
 versionDir="$module/pkg"
